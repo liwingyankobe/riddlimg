85f07f65a972442be2045fe2f82f793732bd2371
ty7e <61668943+ty7e@users.noreply.github.com>
2024-04-12 22:07:00 +0200

    Code uses both space and tab as tabulation, this causes the lines to be offset in some IDEs. Replaced 4x U+0020 SPACE with U+0009 TAB.

--- riddlimg.js
+++ riddlimg.js
@@ -577,45 +577,45 @@ function yandex(){
 function viewChannels(channel){
 
 	//show or hide alpha checkbox
-    const wrapper = document.querySelector('#channelPanel .wrapper');
-    const alphaCheckbox = document.getElementById('channelA');
-    const alphaCheckboxLabel = wrapper.querySelector('label[for="channelA"]');
-    if (document.getElementById('alphaContainer').style.display == 'inline') {
-        wrapper.style.setProperty("--columns", 4);
-        alphaCheckbox.style.display = 'block';
-        alphaCheckboxLabel.style.display = 'block';
-    } else {
-        wrapper.style.setProperty("--columns", 3);
-        alphaCheckbox.style.display = 'none';
-        alphaCheckboxLabel.style.display = 'none';
-    }
+	const wrapper = document.querySelector('#channelPanel .wrapper');
+	const alphaCheckbox = document.getElementById('channelA');
+	const alphaCheckboxLabel = wrapper.querySelector('label[for="channelA"]');
+	if (document.getElementById('alphaContainer').style.display == 'inline') {
+		wrapper.style.setProperty("--columns", 4);
+		alphaCheckbox.style.display = 'block';
+		alphaCheckboxLabel.style.display = 'block';
+	} else {
+		wrapper.style.setProperty("--columns", 3);
+		alphaCheckbox.style.display = 'none';
+		alphaCheckboxLabel.style.display = 'none';
+	}
 
 	//enable or disable channel if chosen
-    if (channel) {
-        channels[channel] = document.getElementById('channel' + channel.toUpperCase()).checked;
-    } else {
-        showPanel('channelPanel');
-    }
+	if (channel) {
+		channels[channel] = document.getElementById('channel' + channel.toUpperCase()).checked;
+	} else {
+		showPanel('channelPanel');
+	}
 
-    currentImageData = structuredClone(originalImageData);
+	currentImageData = structuredClone(originalImageData);
 	let pixels = currentImageData.data;
 
-    if (!channels['r'] && !channels['g'] && !channels['b'] && channels['a']) {
+	if (!channels['r'] && !channels['g'] && !channels['b'] && channels['a']) {
 		//only alpha is selected - show alpha as grayscale
-        for (let i = 0; i < pixels.length; i += 4) {
-            pixels[i]     = 255;
-            pixels[i + 1] = 255;
-            pixels[i + 2] = 255;
-        }
-    } else {
+		for (let i = 0; i < pixels.length; i += 4) {
+			pixels[i]	 = 255;
+			pixels[i + 1] = 255;
+			pixels[i + 2] = 255;
+		}
+	} else {
 		//show enabled channels without processing
-        for (let i = 0; i < pixels.length; i += 4) {
-            pixels[i]     *= channels['r'] ? 1 : 0;
-            pixels[i + 1] *= channels['g'] ? 1 : 0;
-            pixels[i + 2] *= channels['b'] ? 1 : 0;
-            pixels[i + 3]  = channels['a'] ? pixels[i + 3] : 255;
-        }
-    }
+		for (let i = 0; i < pixels.length; i += 4) {
+			pixels[i]	 *= channels['r'] ? 1 : 0;
+			pixels[i + 1] *= channels['g'] ? 1 : 0;
+			pixels[i + 2] *= channels['b'] ? 1 : 0;
+			pixels[i + 3]  = channels['a'] ? pixels[i + 3] : 255;
+		}
+	}
 
 	draw(currentImageData);
 }
@@ -634,40 +634,40 @@ function hasAlphaChannel(imageData) {
 //view RGB(A) bit planes
 function changeBitPlane(channelStep, planeStep){
 	if (channelStep === 0 && planeStep === 0) showPanel('bitPlanePanel');
-    currentImageData = structuredClone(originalImageData);
+	currentImageData = structuredClone(originalImageData);
 
 	//select bit planes
-    const channelCount = (document.getElementById('alphaContainer').style.display == 'inline') ? 5 : 4;
-    const channelName = ['Red', 'Green', 'Blue', 'RGB'].concat(channelCount === 5 ? ['Alpha'] : []);
-    bitPlaneChannel = (bitPlaneChannel + channelStep + channelCount) % channelCount;
-    bitPlane = (bitPlane + planeStep + 8) % 8;
+	const channelCount = (document.getElementById('alphaContainer').style.display == 'inline') ? 5 : 4;
+	const channelName = ['Red', 'Green', 'Blue', 'RGB'].concat(channelCount === 5 ? ['Alpha'] : []);
+	bitPlaneChannel = (bitPlaneChannel + channelStep + channelCount) % channelCount;
+	bitPlane = (bitPlane + planeStep + 8) % 8;
 	document.getElementById('bitPlaneChannel').innerText = channelName[bitPlaneChannel];
-    document.getElementById('bitPlane').innerText = bitPlane.toString();
+	document.getElementById('bitPlane').innerText = bitPlane.toString();
 	
 	//compute bit plane image
 	let pixels = currentImageData.data;
 	for (let i = 0; i < pixels.length; i += 4) {
-        let bitValue;
-        //single RGB channel bit
-        if (bitPlaneChannel < 3) {
-            bitValue = (pixels[i + bitPlaneChannel] >> bitPlane) & 1;
-        }
+		let bitValue;
+		//single RGB channel bit
+		if (bitPlaneChannel < 3) {
+			bitValue = (pixels[i + bitPlaneChannel] >> bitPlane) & 1;
+		}
 
-        for (let j = 0; j < 3; j++) {
-            if (bitPlaneChannel === 3) {
+		for (let j = 0; j < 3; j++) {
+			if (bitPlaneChannel === 3) {
 				//all of RGB channel bits
-                bitValue = (pixels[i + j] >> bitPlane) & 1;
-            } else if (bitPlaneChannel === 4) {
+				bitValue = (pixels[i + j] >> bitPlane) & 1;
+			} else if (bitPlaneChannel === 4) {
 				//alpha channel bit
-                bitValue = (pixels[i + 3] >> bitPlane) & 1;
-            }
+				bitValue = (pixels[i + 3] >> bitPlane) & 1;
+			}
 			//output 0 or 255 color values
-            pixels[i + j] = bitValue ? 255 : 0;
-        }
+			pixels[i + j] = bitValue ? 255 : 0;
+		}
 		if (bitPlaneChannel === 4) {
-            pixels[i + 3] = 255;
-        }
-    }
+			pixels[i + 3] = 255;
+		}
+	}
 	draw(currentImageData);
 }
 
@@ -1000,10 +1000,10 @@ function viewFrame(step) {
 	frameNo = (frameNo + step + gifLength) % gifLength;
 	framesData.move_to(frameNo);
 	const currentFrame = framesData.get_frames()[frameNo];
-    
+	
 	//export frame data to canvas
 	draw(currentFrame.data);
-    
+	
 	const frameNoDisplay = frameNo + 1;
 	document.getElementById('frameNo').innerText = frameNoDisplay.toString();
 	document.getElementById('frameDur').innerText = currentFrame.delay * 10 + "ms";
@@ -1224,9 +1224,9 @@ function displayColorCounts() {
 	const colorCounts = colorCounter();
 	
 	const sortedColorCounts = new Map([...colorCounts.entries()].sort((a, b) => {
-    if (a[1] < b[1]) return 1;
-    if (a[1] > b[1]) return -1;
-    return 0;
+		if (a[1] < b[1]) return 1;
+		if (a[1] > b[1]) return -1;
+		return 0;
 	}));
 
 	colorCounterData = JSON.stringify([...sortedColorCounts]);
@@ -1245,32 +1245,32 @@ function displayColorCounts() {
 //create color entry div for the color counter panel
 function createColorEntry(color, count) {
 
-    const colorEntry = document.createElement('div');
-    colorEntry.classList.add('colorEntry');
+	const colorEntry = document.createElement('div');
+	colorEntry.classList.add('colorEntry');
 
-    const colorSquare = document.createElement('div');
-    colorSquare.classList.add('colorSquare');
-    colorSquare.style.backgroundColor = `rgba(${color})`;
+	const colorSquare = document.createElement('div');
+	colorSquare.classList.add('colorSquare');
+	colorSquare.style.backgroundColor = `rgba(${color})`;
 
-    const colorText = document.createElement('span');
-    colorText.textContent = color;
+	const colorText = document.createElement('span');
+	colorText.textContent = color;
 
-    const countText = document.createElement('span');
+	const countText = document.createElement('span');
 	countText.classList.add('count');
-    countText.textContent = "Count: " + count;
+	countText.textContent = "Count: " + count;
 
-    colorEntry.appendChild(colorSquare);
-    colorEntry.appendChild(colorText);
-    colorEntry.appendChild(document.createElement('br'));
-    colorEntry.appendChild(countText);
+	colorEntry.appendChild(colorSquare);
+	colorEntry.appendChild(colorText);
+	colorEntry.appendChild(document.createElement('br'));
+	colorEntry.appendChild(countText);
 
-    return colorEntry;
+	return colorEntry;
 }
 
 //calculate color counts Map
 function colorCounter() {
 	const pixels = currentImageData.data.reduce((acc, _, i, arr) => {
-    
+	
 		if (i % 4 === 0) {
 			acc.push(arr.slice(i, i + 4).join(", "));
 		}
@@ -1278,7 +1278,7 @@ function colorCounter() {
 	}, []);
 
 	return pixels.reduce((acc, color) => {
-    
+	
 		acc.set(
 			color, 
 			(acc.get(color) || acc.has(color)) + 1
